8b3e7da0a6623a81a5db67699553a6284e7d3547
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-01 14:30:20 +0200

    added routes & RouterModule in main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpModule} from "@angular/http";
+import { HttpModule } from "@angular/http";
+import {
+  Routes,
+  RouterModule
+} from "@angular/router";
 
 import { AppComponent }  from './components/app.component';
 import { SearchComponent }  from './components/search.component';
@@ -8,10 +12,16 @@ import { SearchComponent }  from './components/search.component';
 import {DataService} from "./services/data.service";
 
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+];
+
 @NgModule({
   imports:      [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>
-              <search></search>
+            <router-outlet></router-outlet>
 `,
 })
 export class AppComponent  { name = 'Angular'; }
